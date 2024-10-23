ab3e73e124507cad3c3ae5699eafb546effcccf3
sara-altaf44 <sarawaraich44@gmail.com>
2024-10-23 00:13:33 -0700

    Your descriptive message here

--- src/data/user.js
+++ src/data/user.js
@@ -38,7 +38,7 @@ const INFO = {
 			title: "Project 1",
 			description:
 				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://media.licdn.com/dms/image/v2/D4E2DAQEU-oPc9dubiA/profile-treasury-image-shrink_800_800/profile-treasury-image-shrink_800_800/0/1728802695831?e=1729407600&v=beta&t=I2Fho2P7EibKQuObEGDrORDZ9aDkkAm6v65CmyDnqbM",
+			logo: "https://media.licdn.com/dms/image/v2/D4E2DAQGs9xgPSHMWIA/profile-treasury-image-shrink_800_800/profile-treasury-image-shrink_800_800/0/1728802872031?e=1730275200&v=beta&t=TZI-ku3pU3v_D0czE0iRY_Vv4DumC71G6Es1Rs-COE0",
 			linkText: "View Project",
 			link: "#",
 		},
@@ -47,7 +47,7 @@ const INFO = {
 			title: "Project 2",
 			description:
 				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://media.licdn.com/dms/image/v2/D4E2DAQH7xDhm2uUYcQ/profile-treasury-image-shrink_800_800/profile-treasury-image-shrink_800_800/0/1728802926872?e=1729411200&v=beta&t=gDI02nune6K4KGYmrRgSJOtLBGrDrz4VWJwfLbwpswM",
+			logo: "https://media.licdn.com/dms/image/v2/D4E2DAQGs9xgPSHMWIA/profile-treasury-image-shrink_800_800/profile-treasury-image-shrink_800_800/0/1728802872031?e=1730275200&v=beta&t=TZI-ku3pU3v_D0czE0iRY_Vv4DumC71G6Es1Rs-COE0",
 			linkText: "View Project",
 			link: "#",
 		},
@@ -56,7 +56,7 @@ const INFO = {
 			title: "Project 3",
 			description:
 				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://media.licdn.com/dms/image/v2/D4E2DAQHJaDs08m4dqA/profile-treasury-image-shrink_800_800/profile-treasury-image-shrink_800_800/0/1728802975737?e=1729411200&v=beta&t=JUktPPg6IXtjAhxLLJo0HDqJ_EXIs-xOZ6TNaevCssw",
+			logo: "https://media.licdn.com/dms/image/v2/D4E2DAQH7xDhm2uUYcQ/profile-treasury-image-shrink_800_800/profile-treasury-image-shrink_800_800/0/1728802926872?e=1730275200&v=beta&t=kQK9y5bdDhnjE9gGF4sLIy5ijRqFDqcXeFDvYmDCjm8",
 			linkText: "View Project",
 			link: "#",
 		},
@@ -65,7 +65,7 @@ const INFO = {
 			title: "Project 4",
 			description:
 				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://media.licdn.com/dms/image/v2/D4E2DAQGs9xgPSHMWIA/profile-treasury-image-shrink_800_800/profile-treasury-image-shrink_800_800/0/1728802872031?e=1729411200&v=beta&t=6K2QIfmlQzU3Yulu76GSdIUmOknA-eJnKdOBDDgMCRg",
+			logo: "https://media.licdn.com/dms/image/v2/D4E2DAQH0lDK9K0SImQ/profile-treasury-image-shrink_800_800/profile-treasury-image-shrink_800_800/0/1728803016019?e=1730275200&v=beta&t=tYEUBs-Qt6i85QqbKUfo5tCfkSMXQbS0ORqXn6p1Kdg",
 			linkText: "View Project",
 			link: "#",
 		},
@@ -74,7 +74,7 @@ const INFO = {
 			title: "Project 5",
 			description:
 				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://media.licdn.com/dms/image/v2/D4E2DAQH0lDK9K0SImQ/profile-treasury-image-shrink_800_800/profile-treasury-image-shrink_800_800/0/1728803016019?e=1729411200&v=beta&t=nL_fdE-rqS2t_DJIYqSjquuZ7wACsDZDc6ClxMQs5Kw",
+			logo: "https://media.licdn.com/dms/image/v2/D4E2DAQHJaDs08m4dqA/profile-treasury-image-shrink_800_800/profile-treasury-image-shrink_800_800/0/1728802975737?e=1730275200&v=beta&t=fhMjQP3ti8Be30ngBA3FEzl0j0e_ANHPeeQR7xYYeOM",
 			linkText: "View Project",
 			link: "#",
 		},
